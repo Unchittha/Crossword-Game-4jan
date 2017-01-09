0e8b4e50bca8aefd1a86d26804412db2a996fbfd
praew <praewlove15@gmail.com>
2017-01-09 22:27:16 +0700

    work9jan

--- app/src/main/java/rtc/patcharee/unchittha/crosswordgame/MyConstant.java
+++ app/src/main/java/rtc/patcharee/unchittha/crosswordgame/MyConstant.java
@@ -13,11 +13,11 @@ public class MyConstant {
     public String[][] getVocapStrings() {
 
         vocapStrings = new String[4][2];
-        vocapStrings[0][0] = "1234";
+        vocapStrings[0][0] = "clam";
         vocapStrings[1][0] = "2222";
         vocapStrings[2][0] = "3333";
         vocapStrings[3][0] = "4444";
-        vocapStrings[0][1] = "3210";
+        vocapStrings[0][1] = "slug";
         vocapStrings[1][1] = "3233";
         vocapStrings[2][1] = "4344";
         vocapStrings[3][1] = "5455";
@@ -28,11 +28,11 @@ public class MyConstant {
     public String[][] getWordStrings() {
 
         wordStrings = new String[4][2];
-        wordStrings[0][0] = "ปู";
+        wordStrings[0][0] = "หอยทาก";
         wordStrings[1][0] = "10";
         wordStrings[2][0] = "20";
         wordStrings[3][0] = "30";
-        wordStrings[0][1] = "ปลา";
+        wordStrings[0][1] = "หอยกาบ";
         wordStrings[1][1] = "11";
         wordStrings[2][1] = "21";
         wordStrings[3][1] = "31";
